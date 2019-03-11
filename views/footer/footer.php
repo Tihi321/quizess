8bb7bedb103eb171f97b5ece12159496fc269377
Tihomir Selak <tihomir.selak@infinum.hr>
2019-03-11 21:42:50 +0100

    Add footer styling

--- views/footer/footer.php
+++ views/footer/footer.php
@@ -22,55 +22,52 @@
 
   </div><!-- #content -->
 
-  <footer class="quizess-footer">
-    <?php if ( ! empty( $logo_decoded ) ) { ?>
+  <footer class="quizess-footer quizess-footer--<?php echo esc_attr( $theme ); ?>">
     <div class="quizess-footer__brand">
-      <img src="<?php echo esc_url( $logo_decoded['url'] ); ?>" />
-    </div>
-    <?php } ?>
-    <?php if ( ! empty( $copyright ) ) { ?>
-    <div class="quizess-footer__copyright">
-      <?php echo esc_html( $copyright ); ?>
+      <?php if ( ! empty( $logo_decoded ) ) { ?>
+      <div class="quizess-footer__logo">
+        <img src="<?php echo esc_url( $logo_decoded['url'] ); ?>" />
+      </div>
+      <?php } ?>
+      <?php if ( ! empty( $copyright ) ) { ?>
+      <div class="quizess-footer__copyright">
+        <?php echo esc_html( $copyright ); ?>
+      </div>
     </div>
     <?php } ?>
     <ul class="quizess-footer__social">
     <?php if ( ! empty( $facebook ) ) { ?>
-    <li class="quizess-footer__facebook">
-      <a href="<?php echo esc_url( $facebook ); ?>">
-        <i class="icon--qz-facebook"></i>
-      <?php esc_html_e( 'Facebook', 'quizess' ); ?>
+    <li class="quizess-footer__social-item">
+      <a href="<?php echo esc_url( $facebook ); ?>" aria-label="<?php esc_html_e( 'Facebook', 'quizess' ); ?>">
+        <i class="icon icon--qz-facebook"></i>
       </a>
     </li>
     <?php } ?>
     <?php if ( ! empty( $twitter ) ) { ?>
-    <li class="quizess-footer__twitter">
-      <a href="<?php echo esc_url( $twitter ); ?>">
-        <i class="icon--qz-twitter"></i>
-        <?php esc_html_e( 'Twitter', 'quizess' ); ?>
+    <li class="quizess-footer__social-item">
+      <a href="<?php echo esc_url( $twitter ); ?>" aria-label="<?php esc_html_e( 'Twitter', 'quizess' ); ?>">
+        <i class="icon icon--qz-twitter"></i>
       </a>
     </li>
     <?php } ?>
     <?php if ( ! empty( $linked_in ) ) { ?>
-    <li class="quizess-footer__linkedin">
-       <a href="<?php echo esc_url( $linked_in ); ?>">
-       <i class="icon--qz-linkedin"></i>
-        <?php esc_html_e( 'LinkedIn', 'quizess' ); ?>
+    <li class="quizess-footer__social-item">
+       <a href="<?php echo esc_url( $linked_in ); ?>" aria-label="<?php esc_html_e( 'LinkedIn', 'quizess' ); ?>">
+       <i class="icon icon--qz-linkedin"></i>
       </a>
     </li>
     <?php } ?>
     <?php if ( ! empty( $instagram ) ) { ?>
-    <li class="quizess-footer__instagram">
-      <a href="<?php echo esc_url( $instagram ); ?>">
-        <i class="icon--qz-instagram"></i>
-        <?php esc_html_e( 'Instagram', 'quizess' ); ?>
+    <li class="quizess-footer__social-item">
+      <a href="<?php echo esc_url( $instagram ); ?>" aria-label="<?php esc_html_e( 'Instagram', 'quizess' ); ?>">
+        <i class="icon icon--qz-instagram"></i>
       </a>
     </li>
     <?php } ?>
     <?php if ( ! empty( $github ) ) { ?>
-    <li class="quizess-footer__github">
-      <a href="<?php echo esc_url( Config::GITHUB_URL ); ?>">
-        <i class="icon--qz-github"></i>
-        <?php esc_html_e( 'Github', 'quizess' ); ?>
+    <li class="quizess-footer__social-item">
+      <a href="<?php echo esc_url( Config::GITHUB_URL ); ?>" aria-label="<?php esc_html_e( 'Github', 'quizess' ); ?>">
+        <i class="icon icon--qz-github"></i>
       </a>
     </li>
     <?php } ?>
