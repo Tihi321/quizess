ccbcd0d8067006fce9cf458cba0b68de5e3ecec8
Tihomir Selak <tihomir.selak@infinum.hr>
2019-03-16 22:51:26 +0100

    Add menu icon

--- views/header/header-content.php
+++ views/header/header-content.php
@@ -6,6 +6,10 @@
  * @since 1.0.0
  */
 
+use Quizess\Helpers\General_Helper;
+
+$base_path = General_Helper::get_base_path();
+
 ?>
 <header class="quizess-header quizess-header--<?php echo esc_attr( $theme ); ?> js-header">
   <div class="quizess-header__overlay js-header-overlay">
@@ -14,7 +18,15 @@
     <div class="quizess-header__menu js-header-menu" data-theme="<?php echo esc_attr( $theme ); ?>">
     </div>
     <div class="quizess-header__toggle js-menu-toggle">
-      <span class="quizess-header__toggle-text">Menu</span>
+      <span class="quizess-header__toggle-icon">
+      <?php
+      $menu_icon = $base_path . 'views/svg/menu-icon.php';
+
+      if ( ! empty( $menu_icon ) ) {
+        include $menu_icon;
+      }
+      ?>
+      </span>
   </div>
   </div>
 </header>
