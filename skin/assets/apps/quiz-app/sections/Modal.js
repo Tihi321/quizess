53b827b021b13b945a2b60f3931be1e5816e7d7c
Tihomir Selak <tihomir.selak@infinum.hr>
2019-03-16 17:49:33 +0100

    Add exit messages

--- skin/assets/apps/quiz-app/sections/Modal.js
+++ skin/assets/apps/quiz-app/sections/Modal.js
@@ -28,10 +28,26 @@ const ModalConsumer = (props) => {
   const modalInnerClasses = classnames(
     'modal__inner',
     `modal__inner--${theme}`,
-    {'modal__inner--exit': showExit},
+    (showExit) ? 'modal__inner--wide' : 'modal__inner--narrow',
   );
 
   if (showExit) {
+    const messages = [
+      __('Get outta here and go back to your boring programs.', 'quizess'),
+      __('Just leave. When you come back, I\'ll be waiting with a bat.', 'quizess'),
+      __('Are you sure you want to quit this great quiz ?', 'quizess'),
+      __('I wouldn\'t leave if I were you. Internet is much worse.', 'quizess'),
+      __('Go ahead and leave. See if I care.', 'quizess'),
+      __('Choose Cancel if you are brave, choose Exit to cover in shame. ', 'quizess'),
+      __('Chickening out... already ?', 'quizess'),
+      __('Heroes choose Cancel, Wimps choose Exit.', 'quizess'),
+      __('So you think you can quit this easily, huh ?', 'quizess'),
+      __('Dost thou wish to leave with such hasty abandon ?', 'quizess'),
+    ];
+
+    const rnd = Math.floor(Math.random() * Math.floor(10));
+    const message = messages[rnd];
+
     return (
       <div
         className={modalClasses}
@@ -40,7 +56,7 @@ const ModalConsumer = (props) => {
         <div className={modalInnerClasses}>
           <div className="modal__exit-outer">
             <div className="modal__exit-title">
-              {__('Leaving already', 'quizess')}
+              {message}
             </div>
             <div className="modal__exit-btns">
               <Button
---
+++ skin/assets/components/modal/overview/index.js
@@ -0,0 +1,103 @@
+import {__} from '@wordpress/i18n';
+import {WideButton} from '../../index';
+import generalHelper from '../../../helpers/general-helper';
+
+const Overview = (props) => {
+  const {
+    theme,
+    onClick,
+    questionsTotal,
+    correctAnswers,
+    questionStats,
+  } = props;
+
+  const percentSuccess = generalHelper.getPercentage(correctAnswers, questionsTotal);
+
+  const statsElements = questionStats.map((item, index) => {
+    const {
+      id,
+      correct,
+    } = item;
+    return (
+      <li key={index} className="stats__item">
+        <div className="stats__inner--overview">
+          {index + 1}.
+        </div>
+        <div className="stats__inner--overview">
+          {id}.
+        </div>
+        <div className="stats__inner--overview">
+          {(correct) ? __('Correct', 'quizess') : __('Incorrect', 'quizess')}
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <div className="overview">
+      <div className="overview__header">
+        {__('Kudos !', 'quizess')}
+      </div>
+      <WideButton
+        theme={theme}
+        onClick={onClick}
+      >
+        {__('Try again', 'quizess')}
+      </WideButton>
+      <div className="overview__footer">
+        <div className="quiz-accomplishment">
+          <div className="modal__table-title quiz-accomplishment__title">
+            {__('Stats', 'quizess')}
+          </div>
+          <ul className="quiz-accomplishment__parent">
+            <li className="quiz-accomplishment__item quiz-accomplishment__item-title">
+              <div className="quiz-accomplishment__inner quiz-accomplishment__title">
+                {__('Correct', 'quizess')}
+              </div>
+              <div className="quiz-accomplishment__inner quiz-accomplishment__title">
+                {__('Total', 'quizess')}
+              </div>
+              <div className="quiz-accomplishment__inner quiz-accomplishment__title">
+                {__('Success', 'quizess')}
+              </div>
+            </li>
+            <li className="quiz-accomplishment__item">
+
+              <div className="quiz-accomplishment__inner">
+                {correctAnswers}
+              </div>
+              <div className="quiz-accomplishment__inner">
+                {questionsTotal}
+              </div>
+              <div className="quiz-accomplishment__inner">
+                {`${percentSuccess}%`}
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="modal__stats-overview">
+          <div className="modal__table-title stats__table-title">
+            {__('Overview', 'quizess')}
+          </div>
+          <ul className="stats__parent">
+            <li className="stats__item stats__item-title">
+              <div className="stats__inner--overview stats__title">
+                {__('Question', 'quizess')}
+              </div>
+              <div className="stats__inner--overview stats__title">
+                {__('Answer', 'quizess')}
+              </div>
+              <div className="stats__inner--overview stats__title">
+                {__('Status', 'quizess')}
+              </div>
+            </li>
+            {statsElements}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+export default Overview;
