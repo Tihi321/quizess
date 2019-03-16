4c43f11ab5e4a2500c2a5a37926606124dac78a3
Tihomir Selak <tihomir.selak@infinum.hr>
2019-03-16 17:58:35 +0100

    Add stats & accomplisment table style

--- views/modal/modal-about.php
+++ views/modal/modal-about.php
@@ -9,7 +9,7 @@
 ?>
 
 <div class="modal js-modal" id="<?php echo esc_attr( $about_modal_id ); ?>" style="background-color:<?php echo esc_attr( $bg_color ); ?>;background-image:url('<?php echo esc_attr( $bg_image_url ); ?>');">
-  <div class="modal__inner modal__inner--<?php echo esc_attr( $theme ); ?>">
+  <div class="modal__inner modal__inner--narrow modal__inner--<?php echo esc_attr( $theme ); ?>">
     <div class="modal__top-bar modal__top-bar--<?php echo esc_attr( $theme ); ?>">
       <h1 class="modal__title">
         <?php echo esc_html__( 'About', 'quizess' ); ?>
