5ce96c0423a1633f248d86434e68fe65c69c19ad
Tihomir Selak <tihomir.selak@infinum.hr>
2019-09-13 03:16:55 +0200

    bump version

--- quizess.php
+++ quizess.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Quizess
  * Plugin URI:
  * Description:       Quizess plugin provides all functionality for making quizes.
- * Version:           1.0.2
+ * Version:           2.0.0
  * Author:            Tihomir Selak <tknox.dr@gmail.com>
  * Author URI:        https://tihomir-selak.from.hr/
  * License:           GPL-2.0+
